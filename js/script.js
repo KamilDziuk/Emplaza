11d0ce68431bbeb654feedced5c832f9bc0d61ed
kamildziuk <kontakt@kamildziuk.pl>
2024-08-10 18:36:17 +0200

    Added slider. Added text. Added image in browser icon. Added image in main text. Improved header and footer appearance. Added button with PDF file. Added new images(office1-9)

--- js/script.js
+++ js/script.js
@@ -19,3 +19,42 @@ window.addEventListener("scroll", () =>
 let nav_headar = document.querySelector(".nav_headar");
 nav_headar.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+let galleryItem = document.querySelectorAll('.menuImage');
+let numbers = 0;
+
+function  gallery(index)
+{
+    galleryItem.forEach((item, i) => {
+        item.style.transform = `translateX(${(i - index)* 123}%)`;
+        item.style.opacity = i  === index ? 1: 0.3;
+    });
+
+    
+};
+
+
+
+
+
+
+setInterval( () =>
+{
+    numbers = (numbers < galleryItem.length -1) ?  numbers +1:0;
+    gallery(numbers);
+
+},3000)
+
+
+document.querySelector('.left').addEventListener('click', () => {
+numbers = (numbers < galleryItem.length - 1)? numbers + 1: 0;
+gallery(numbers);
+});
+
+document.querySelector('.right').addEventListener('click', () => {
+numbers = (numbers > 0) ? numbers -1: galleryItem.length -1;
+gallery(numbers);
+});
+
+gallery(numbers);
